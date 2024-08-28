97893174a2b14907a1577d7e727cb6dc87fe6b75
Andrew Piper <andrew@threax.com>
2024-08-28 08:49:59 -0400

    Setup test.

--- OpenAlTestConsole/OpenAlTestConsole.cpp
+++ OpenAlTestConsole/OpenAlTestConsole.cpp
@@ -1,20 +1,16 @@
 // OpenAlTestConsole.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include "al.h" 
+#include "alc.h"
+#include "alext.h"
+
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //This doesn't have to actually do anything, the error is loading the library
+    auto device = alcOpenDevice(NULL);
+
+    std::cout << "Device created\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
